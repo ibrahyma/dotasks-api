df53b9b7b493498aaf6d66d7c234a8d9a7331a08
ibrahyma <36759387+ibrahyma@users.noreply.github.com>
2024-04-18 18:23:05 +0200

    Create project

---
+++ src/main/java/com/ibrahyma/dotasksapi/DotasksApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahyma.dotasksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotasksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotasksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahyma/dotasksapi/DotasksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahyma.dotasksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotasksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
