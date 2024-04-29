2f44f6d1f549f38c5cc42e5b3749eaf92643c5bc
ibrahyma <36759387+ibrahyma@users.noreply.github.com>
2024-04-29 18:18:16 +0200

    Create TaskRepository interface

---
+++ src/main/java/com/ibrahyma/dotasksapi/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.ibrahyma.dotasksapi.repository;
+
+import com.ibrahyma.dotasksapi.model.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Integer> {
+}
